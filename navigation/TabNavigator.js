d290a90f748da0054fdcd4e83c93fa0854d13015
Jignesh Kumar <87186589+JigneshKumar506@users.noreply.github.com>
2023-12-08 13:26:39 +0530

    Add files via upload

---
+++ navigation/TabNavigator.js
@@ -0,0 +1,59 @@
+import * as React from 'react';
+import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Feed from '../Screens/Feed';
+import CreatePost from '../Screens/CreatePost';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+const Tab = createMaterialBottomTabNavigator();
+
+const BottomTabNavigator = () => {
+  return (
+    <Tab.Navigator
+      labeled={false}
+      barStyle={styles.bottomTabStyle}
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          if (route.name === 'Feed') {
+            iconName = focused ? 'book' : 'book-outline';
+          } else if (route.name === 'CreatePost') {
+            iconName = focused ? 'create' : 'create-outline';
+          }
+          return <Ionicons name={iconName} size={RFValue(25)} color={color} />;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: 'blue',
+        inactiveTintColor: 'white',
+      }}>
+      <Tab.Screen
+        name="Feed"
+        component={Feed}
+        options={{ headerShown: false }}
+      />
+      <Tab.Screen
+        name="CreatePost"
+        component={CreatePost}
+        options={{ headerShown: false }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+const styles = StyleSheet.create({
+  bottomTabStyle: {
+    backgroundColor: '#2f345d',
+    height: '8%',
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    overflow: 'hidden',
+    position: 'absolute',
+  },
+
+});
+
+export default BottomTabNavigator;
